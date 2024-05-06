7edcec8afd8d4045ee9d19499e251b9fb5ace2f9
Niklas Dörner <88161666+ndoerner@users.noreply.github.com>
2024-05-06 20:08:12 +0200

    Umstellung Residuenberechnung

--- R/ps_optimise.R
+++ R/ps_optimise.R
@@ -14,9 +14,31 @@ ps_optimise <- function(lambda
     L = L,
     keep_data = TRUE
   )
+  weighted_residuals <- function(mp = point_estim$model_par, f = framework, fixed = fixed, lambda = lambda) {
+    split(x = framework$smp_data, f = framework$smp_domains_vec) |>
+      lapply(function(dom) {
+        d <- which(unique(f$smp_domains_vec) %in% unique(dom[[f$smp_domains]]))
+        transformed_y <- box_cox_std(dom[, all.vars(fixed)[1]], lambda)
+        covariates <- dom[all.vars(fixed)[-1]]
+        factors <- vapply(covariates, is.factor, FUN.VALUE = logical(1))
+        covariates[factors] <- vapply(covariates[factors], function(x) {
+          as.numeric(x) - 1
+        }, FUN.VALUE = numeric(nrow(covariates)))
+        covariates <- as.matrix(cbind(1, covariates))
 
-  resid <- resid(point_estim$model, level = 0, type = "pearson")
-  ranef <- nlme::ranef(point_estim$model)[[1]]
+        # Summe aus der Definition nicht relevant, da Skalarprodukt/Punktprodukt
+        x_iw <- sum(dom$weight)^-1 * dom$weight %*% covariates
+        y_iw <- sum(dom$weight)^-1 * dom$weight %*% transformed_y
+
+        fitted <- as.vector(covariates %*% mp$betas) + as.vector(mp$gammaw[d] * (y_iw  - x_iw %*% mp$betas))
+
+        # Return Pearson residuals. sigma_w_2 is based on G. (16)
+        sigma_w_2 <- mp$sigmau2est * (1 - mp$gammaw[d]) + mp$sigmae2est
+        (transformed_y - fitted) / sqrt(sigma_w_2)
+      }) |> unsplit(f = framework$smp_domains_vec)
+  }
+  resid <- weighted_residuals(point_estim$model_par, framework, fixed, lambda) # resid(point_estim$model, level = 0, type = "pearson")
+  ranef <- point_estim$model_par$rand_eff[framework$dist_obs_dom]
 
   sigma2_e_est <- point_estim$model_par$sigmae2est
   sigma2_u_est <- point_estim$model_par$sigmau2est
